687950bf68e9b442a64b1d66b303adfb9b04650c
EmmaD9 <maduprey1@gmail.com>
2026-04-12 23:49:53 -0400

    finished domo a

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -1,5 +1,5 @@
 const models = require('../models');
-const Account = models.Account;
+const {AccountModel} = models.Account;
 
 const loginPage = (req, res) => {
     return res.render('login');
@@ -14,30 +14,43 @@ const logout = (req, res) => {
 };
 
 const login = (req, res) => {
+    const username = req.body.username;
+    const pass = req.body.pass;
 
-};
-
-const signup = (req, res) => {
-    const username = '${req.body.username}';
-    const pass = '${req.body.pass}';
-    const pass2 = '${req.body.pass2}';
-
-    if(!username || !pass || !pass2) {
+    if (!username || !pass) {
         return res.status(400).json({ error: 'All fields are required!' });
     }
 
-    if(pass !== pass2) {
+    return AccountModel.authenticate(username, pass, (err, account) => {
+        if (err || !account) {
+            return res.status(401).json({ error: 'Wrong username or password!' });
+        }
+        req.session.account = account;
+        return res.json({ redirect: '/maker' });
+    });
+};
+
+const signup = async (req, res) => {
+    const username = req.body.username;
+    const pass1 = req.body.pass;
+    const pass2 = req.body.pass2;
+
+    if (!username || !pass1 || !pass2) {
+        return res.status(400).json({ error: 'All fields are required!' });
+    }
+
+    if (pass1 !== pass2) {
         return res.status(400).json({ error: 'Passwords do not match!' });
     }
 
-    try{
-        const hash = await Account.generateHash(pass);
-        const newAccount = new Account ({username, password: hash});
+    try {
+        const hash = await AccountModel.generateHash(pass1);
+        const newAccount = new AccountModel({ username, password: hash });
         await newAccount.save();
-        return res.json({redirect: '/maker'});
+        return res.json({ redirect: '/maker' });
     } catch (err) {
         console.log(err);
-        if(err.code === 11000) {
+        if (err.code === 11000) {
             return res.status(400).json({ error: 'Username already in use!' });
         }
         return res.status(500).json({ error: 'An error occured!' });
