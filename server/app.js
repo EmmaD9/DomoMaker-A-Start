7457254778804de43afe4ec562796df848b3ed46
EmmaD9 <maduprey1@gmail.com>
2026-04-17 12:34:43 -0400

    up to step 9

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
@@ -22,32 +24,45 @@ mongoose.connect(dbURI).catch((err) => {
     }
 });
 
-
-const app = express();
-
-app.use(helmet());
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(session({
-  key: 'sessionid',
-  secret: 'Domo Arigato',
-  resave: false,
-  saveUninitialized: false,
-}));
-
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views`);
-
-router(app);
-
-app.listen(port, (err) => {
-    if (err) { throw err; }
-    console.log(`Listening on port ${port}`);
+const redisClient = redis.createClient({
+    url: process.env.REDIS_URL,
 });
+redisClient.on('error', (err) => {
+    console.log('Redis Client Error', err);
+});
+
+redisClient.connect().then(() => {
+    const app = express();
+
+    app.use(helmet());
+    app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
+    app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+    app.use(compression());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+
+    app.use(session({
+        key: 'sessionid',
+        store: new RedisStore({
+            client: redisClient,   
+        }),
+        secret: 'Domo Arigato',
+        resave: false,
+        saveUninitialized: false,
+    }));
+
+    app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+    app.set('view engine', 'handlebars');
+    app.set('views', `${__dirname}/../views`);
+
+    router(app);
+
+    app.listen(port, (err) => {
+        if (err) { throw err; }
+        console.log(`Listening on port ${port}`);
+    });
+});
+
+
